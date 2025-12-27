3d76456b458cd915ae5e3d732853f87774b76127
Nikhil Mahankali <nikhilmahankali56@gmail.com>
2025-12-27 11:04:07 -0800

    Integrate interpreter to run AST after parsing

--- runner.py
+++ runner.py
@@ -1,6 +1,6 @@
-from lexer import lex, Token
-from parser import Parser, ProgramNode, NumberNode, BinOpNode, VarNode, AssignNode, PrintNode
-
+from lexer import lex
+from parser import Parser
+from interpreter import Interpreter
 # Example code as if it were in code.txt
 code_lines = []
 with open("code.txt", 'r') as file:
@@ -10,12 +10,9 @@ with open("code.txt", 'r') as file:
 # 1. Tokenize the code
 tokens = lex(code_lines)
 
-# Optional: append EOF token if lexer doesn't
-tokens.append(Token("EOF", "", -1, -1))
-
 # 2. Parse tokens into an AST
 parser = Parser(tokens)
 ast = parser.program()
 
-# 3. Print the resulting AST
-print(ast)
+interpreter = Interpreter()
+interpreter.run(ast)
