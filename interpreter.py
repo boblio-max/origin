af0584f498b8c9fa3f96ccee43667e3b97bfa165
Nikhil Mahankali <nikhilmahankali56@gmail.com>
2026-01-04 10:15:03 -0800

    Enhance interpreter to handle class nodes
    Added support for class definitions and automatic __init__ method generation.

--- interpreter.py
+++ interpreter.py
@@ -1,6 +1,10 @@
+from math import e
 from classes import *
 
 class Interpreter:
+    def __init__(self):
+        self.global_classes = set()
+
     def generate(self, node):
         if isinstance(node, ProgramNode):
             return "\n".join(self.generate(stmt) for stmt in node.statements)
@@ -8,8 +12,34 @@ class Interpreter:
         elif isinstance(node, BlockNode):
             return "\n".join(self.generate(stmt) for stmt in node.statements)
         
+        elif isinstance(node, FuncNode):
+            if hasattr(node, "params") and node.params:
+                params_code = ", ".join(node.params)
+                code = f"def {node.name}({params_code}):\n"
+            else:
+                code = f"def {node.name}():\n"
+
+            body = self.indent_block(self.generate(node.body))
+            code += body
+            return code
+        
+        elif isinstance(node, CallNode):
+            args_code = ", ".join(self.generate(a) for a in node.arg) if node.arg else ""
+            if isinstance(node.func_name, VarNode):
+                if node.func_name.name in self.global_classes:  # you can define self.global_classes = set of class names
+                    return f"{node.func_name.name}({args_code})"
+
+            return f"{self.generate(node.func_name)}({args_code})"
+
+            
         elif isinstance(node, AssignNode):
             return f"{node.name} = {self.generate(node.value)}"
+        elif isinstance(node, IndexAssignNode):
+            collection = self.generate(node.collection)
+            index = self.generate(node.index)
+            value = self.generate(node.value)
+            return f"{collection}[{index}] = {value}"
+
         elif isinstance(node, PrintNode):
             return f"print({self.generate(node.expr)})"
 
@@ -18,9 +48,30 @@ class Interpreter:
 
         elif isinstance(node, StringNode):
             return repr(node.value)
-
+        
+                
         elif isinstance(node, VarNode):
             return node.name
+
+        elif isinstance(node, ClassNode):
+            code = f"class {node.name}:\n"
+            self.global_classes.add(node.name)
+            # fields: create in __init__ if not already defined
+            init_node = node.methods.get("init", None)
+            if init_node is None and node.fields:
+                # create default __init__ if none
+                params_code = ", ".join(node.fields)
+                init_code = f"def __init__(self, {params_code}):\n"
+                for field in node.fields:
+                    init_code += f"    self.{field} = {field}\n"
+                code += self.indent_block(init_code)
+            else:
+                # include existing methods including init
+                for method in node.methods.values():
+                    method_code = self.generate(method)
+                    code += self.indent_block(method_code)
+
+            return code
 
         elif isinstance(node, ListNode):
             return f"[{', '.join(self.generate(el) for el in node.elements)}]"
